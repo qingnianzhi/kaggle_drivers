54f949115aebb245e4831f8979cf033b75b502f8
qingnianzhi <qingnianzhi@sina.com>
2016-06-23 11:46:30 +0800

    可以按照人分出train&&valid也可以随机分割
    增加了随机分割

--- get_driver_train_valid_label.py
+++ get_driver_train_valid_label.py
@@ -1,49 +1,81 @@
 #!/usr/bin/env python2
 #coding:utf-8
 
-
+import sys
 import os
 import random
 
 if __name__ == '__main__':
+    print 'This program is used for get train and valid label'
+    print 'Use: python get_driver_train_valid.py 0/1'
+    print 'Note 0:shuffle by person 1:shuffle randomly'
 
-    p = ['p002', 'p012', 'p014', 'p015', 'p016', 'p021', 'p022', 'p024', 'p026', 'p035', 'p039', 'p041', 'p042', 'p045',
-         'p047', 'p049', 'p050', 'p051', 'p052', 'p056', 'p061', 'p064', 'p066', 'p072', 'p075', 'p081']
-    random.shuffle(p)
-    valid = p[0:3]
-    print valid
+    if sys.argv[1] == '0':
+        print 'shuffle by person'
+        p = ['p002', 'p012', 'p014', 'p015', 'p016', 'p021', 'p022', 'p024', 'p026', 'p035', 'p039', 'p041', 'p042', 'p045',
+             'p047', 'p049', 'p050', 'p051', 'p052', 'p056', 'p061', 'p064', 'p066', 'p072', 'p075', 'p081']
+        random.shuffle(p)
+        valid = p[0:3]
+        print valid
 
-    f = open('./driver_imgs_list.csv')
-    drivers = f.readlines()[1:]
-    f.close()
-    path_train = []
-    path_valid = []
-    for i in drivers:
-        person, label, img = i.split(',')
-        if person in valid:
-            path_valid.append(['train/'+label+'/'+img[:-1],label[1:]])
-        else:
-            path_train.append(['train/'+label+'/'+img[:-1],label[1:]])
+        f = open('./driver_imgs_list.csv')
+        drivers = f.readlines()[1:]
+        f.close()
+        path_train = []
+        path_valid = []
+        for i in drivers:
+            person, label, img = i.split(',')
+            if person in valid:
+                path_valid.append(['train/'+label+'/'+img[:-1],label[1:]])
+            else:
+                path_train.append(['train/'+label+'/'+img[:-1],label[1:]])
 
-    f = open('./train.txt','w')
-    random.shuffle(path_train)
-    for i in path_train:
-        f.write(i[0])
-        f.write(' ')
-        f.write(i[1])
-        f.write('\n')
-    f.close()
+        f = open('./train.txt','w')
+        random.shuffle(path_train)
+        for i in path_train:
+            f.write(i[0])
+            f.write(' ')
+            f.write(i[1])
+            f.write('\n')
+        f.close()
 
-    f = open('./valid.txt','w')
-    random.shuffle(path_valid)
-    for i in path_valid:
-        f.write(i[0])
-        f.write(' ')
-        f.write(i[1])
-        f.write('\n')
-    f.close()
+        f = open('./valid.txt','w')
+        random.shuffle(path_valid)
+        for i in path_valid:
+            f.write(i[0])
+            f.write(' ')
+            f.write(i[1])
+            f.write('\n')
+        f.close()
 
+    elif sys.argv[1] == '1':
+        print 'shuflle randomly'
+        f = open('./driver_imgs_list.csv')
+        drivers = f.readlines()[1:]
+        f.close()
+        path = []
+        for i in drivers:
+            person, label, img = i.split(',')
+            path.append(['train/' + label + '/' + img[:-1], label[1:]])
+        random.shuffle(path)
 
+        seg = int(len(path)*0.85)
+        print seg
+        f = open('./train.txt', 'w')
+        for i in path[0:seg]:
+            f.write(i[0])
+            f.write(' ')
+            f.write(i[1])
+            f.write('\n')
+        f.close()
+
+        f = open('./valid.txt', 'w')
+        for i in path[seg:]:
+            f.write(i[0])
+            f.write(' ')
+            f.write(i[1])
+            f.write('\n')
+        f.close()
 
 
 
