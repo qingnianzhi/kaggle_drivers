f91fdbd94ace1c7e0563818d750a76bd261cede9
qingnianzhi <qingnianzhi@sina.com>
2016-07-20 04:04:19 +0800

    fix one bug

--- 2016-7-20/train_8_folds.sh
+++ 2016-7-20/train_8_folds.sh
@@ -5,7 +5,7 @@ for x in 0 1 2 3 4 5 6 7
 do
   python split_train_valid.py $x
   mkdir ./model/log/log$x
-  ~/caffe/build/tools/caffe train --gpu=0 --solver=./model/solver.prototxt --log_dir=./model/log$x --weights=./model/bvlc_googlenet.caffemodel
+  ~/caffe/build/tools/caffe train --gpu=0 --solver=./model/solver.prototxt --log_dir=./model/log/log$x --weights=./model/bvlc_googlenet.caffemodel
   mkdir ./model/model$x
   cp -rf ./model/googlenet/* ./model/model$x/
 done
